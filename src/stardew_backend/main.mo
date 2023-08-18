ba29cee31040aa318fc90644f904e4d30f03b516
bairorb <bairordz@gmail.com>
2023-08-17 22:20:20 -0600

    frontend funcional

--- src/stardew_backend/main.mo
+++ src/stardew_backend/main.mo
@@ -11,19 +11,18 @@ actor {
   type Player = {
     id:Nat; 
     name:Text; 
+    streams:Nat;
+    streams_payed:Nat;
     icp_identity:Principal};
 
+
+  var secretword = "";
   var player_id = 0;
   var buffer_players =  Buffer.Buffer<Player>(0);  
 
-  
-  func isplayer_added(msg:Principal): Bool{  
-    var playerfound = false;
-    Buffer.iterate<Player>(buffer_players, func (x) {   
-      if (x.icp_identity == msg){
-        playerfound := true};
-    });
-    return playerfound
+  public shared(msg) func setword(_word:Text): async Text{
+    secretword := _word;
+    return secretword;
   };
 
   public shared (msg) func add_player(_name : Text): async (){
@@ -32,9 +31,36 @@ actor {
     newplayer.add({
       id = player_id;
       name = _name;
+      streams = 0;
+      streams_payed = 0;
       icp_identity = msg.caller;
     });
-    buffer_players.append(newplayer);
+  buffer_players.append(newplayer);
+  };
+
+  public shared (msg) func add_streamtoplayer(_name : Text, _input_secretword: Text): async Bool{
+    var response = false;
+    if (_input_secretword == secretword){
+      let newplayer = Buffer.Buffer<Player>(0);
+      var buffercount = 0;
+      
+      Buffer.iterate<Player>(buffer_players, func (_player) {   
+        
+        if (_player.name == _name){
+          buffer_players.add({
+            id = _player.id;
+            name = _player.name;
+            streams = _player.streams +1;
+            streams_payed = _player.streams_payed;
+            icp_identity = _player.icp_identity;
+          });
+          let temp = buffer_players.remove(buffercount);
+          response := true;
+        };
+        buffercount += 1;             
+      });
+    };
+    return response;
   };
 
   public query func getPlayers(): async [Player] {
@@ -43,14 +69,14 @@ actor {
 
   public func deleteplayer_byid(idx: Nat) : async Bool  {
     var deleted = false;
-    var index = 0;
+    var counterbuffer = 0;
     
     Buffer.iterate<Player>(buffer_players, func (x) {   
       if (x.id == idx){
         deleted := true;
-        let deleteplayer = buffer_players.remove(index);       
+        let deleteplayer = buffer_players.remove(counterbuffer);       
       };
-      index += 1;
+      counterbuffer += 1;
     });
 
     return deleted
--- src/stardew_frontend/src/index.js
+++ src/stardew_frontend/src/index.js
@@ -1,19 +1,71 @@
 import { stardew_backend } from "../../declarations/stardew_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+const sectionplayers = document.getElementById('players');
+
+function renderplayers(arrayitems){
+  let newHTML = '<ul>';
+  for (let item of arrayitems) { 
+    let id = item['id'];
+    let name = item['name'];
+    let streams = item['streams'];    
+    newHTML += '<li> '+ id +' ' + name + ' ' + streams + '</li>';
+  };
+  newHTML += '</ul>';
+  sectionplayers.innerHTML = newHTML;
+};
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+  e.preventDefault(); 
+  const getplayers = await stardew_backend.getPlayers();
+  renderplayers(getplayers)
+  return false;
+}, false);
+
+document.querySelector("#newplayer").addEventListener("submit", async (e) => {
   e.preventDefault();
+  //console.log("newplayer")
   const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
+  const name = document.getElementById("name_newplayer").value.toString();
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await stardew_backend.greet(name);
-
+  const newplayer = await stardew_backend.add_player(name);
   button.removeAttribute("disabled");
+  
+  //getplayers
+  const getplayers = await stardew_backend.getPlayers();
+  renderplayers(getplayers)
 
-  document.getElementById("greeting").innerText = greeting;
+  return false;
+});
 
+
+document.querySelector("#addstreaming").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+  const name = document.getElementById("name_addstream").value.toString();
+  button.setAttribute("disabled", true);
+
+  const addstream = await stardew_backend.add_streamtoplayer(name);
+  button.removeAttribute("disabled");
+  
+  //getplayers
+  const getplayers = await stardew_backend.getPlayers();
+  renderplayers(getplayers)
+  return false;
+});
+
+
+document.querySelector("#deleteplayer").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+  const id = document.getElementById("id_delete").value.toString();
+  button.setAttribute("disabled", true);
+
+  const addstream = await stardew_backend.deleteplayer_byid(parseInt(id));
+  button.removeAttribute("disabled");
+  
+  //getplayers
+  const getplayers = await stardew_backend.getPlayers();
+  renderplayers(getplayers)
   return false;
 });
